fa9e12075833362dc4d89211f9cec26738759f6c
eko rustanto <mohammadekorustanto21@gmail.com>
2023-02-22 09:52:09 +0700

    web tjkt astrindo

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// togle navbar active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+// jika klik di luar navbar/sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
